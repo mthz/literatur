393ccd853f36eec9b06955b40ed84fff4008888d
Matthias <schoerki@gmail.com>
2016-03-13 03:01:31 +0100

    test travis ci integration

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -12,8 +12,12 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO build > /dev/null
     # go into directory and copy data we're interested in to that directory
     cd build
-    cp ../ros/ros-basics/arra-ros-basics.pdf
-    cp ../ros/ros-build-system/arra-ros-build-system.pdf
+    cp ../ros/ros-basics/arra-ros-basics.pdf .
+    cp ../ros/ros-build-system/arra-ros-build-system.pdf .
+    cp ../ros/ros-tools-roslaunch/arra-ros-tools-roslaunch.pdf .
+    cp ../ros/ros-tools-rqt/arra-ros-tools-rqt.pdf .
+    cp ../ros/ros-tools-rviz/arra-ros-tools-rviz.pdf .
+    cp ../ros/ros-topic-message-service/arra-ros-message-service.pdf .
 
     # add, commit and push files
     git add -f .
